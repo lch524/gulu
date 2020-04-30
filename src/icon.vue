5ddfff06e9d2a0922ce7a66ff818b24a9bab2479
lch524 <295914764@qq.com>
2020-04-30 23:57:28 +0800

    添加了一系列关于icon的功能，实现了settings和loading的点击切换

---
+++ src/icon.vue
@@ -0,0 +1,16 @@
+<template>
+    <svg  class="g-icon">
+        <use v-bind:xlink:href="`#i-${name}`"></use>
+    </svg>
+</template>
+<script>
+export default {
+    props: ['name']
+}
+</script>
+<style>
+    .g-icon{
+        width: 1em;
+        height: 1em;
+    }
+</style>
