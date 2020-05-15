86a0872da0cb3b0a65d773ee8bccd22b55577bfe
lch524 <295914764@qq.com>
2020-05-15 13:49:20 +0800

    重构 row 和 col

--- src/col.vue
+++ src/col.vue
@@ -1,7 +1,7 @@
 <template>
-<div class="col" :class="[`col-${span}`]">
-    <slot></slot>
-</div>
+    <div class="col" :class="colClass" :style="colStyle">
+            <slot></slot>
+    </div>
 </template>
 <script>
 export default {
@@ -9,21 +9,48 @@ export default {
     props: {
         span: {
             type:[Number,String]
+        },
+        offset: {
+            type:[Number,String]
         }
+    },
+    data() {
+        return {
+            gutter: 0
+        }
+    },
+    computed: {
+        colClass() {
+            let {span,offset} = this
+            return [
+                span && `col-${span}`,
+                offset && `offset-${offset}`
+            ]
+        },
+      colStyle(){
+          return {
+              paddingLeft: this.gutter/2+'px',
+              paddingRight: this.gutter/2+'px',
+          }
+      }
     }
 }
 </script>
 <style scoped lang="scss">
     .col{
-        height: 100px;
-        background: lightblue;
         width: 50%;
-        border: 1px solid red;
         $class-prefix: col-;
         @for $n from 1 through 24 {
             &.#{$class-prefix}#{$n} {
-                width: ($n/24)*100%;
+                width: ($n / 24) * 100%;
+            }
+        }
+        $class-prefix: offset-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                margin-left: ($n / 24) * 100%;
             }
         }
     }
+
 </style>
--- src/row.vue
+++ src/row.vue
@@ -1,10 +1,31 @@
 <template>
-<div class="row">
+<div class="row" :style="rowStyle">
     <slot></slot>
 </div>
 </template>
 <script>
-
+export default {
+    name:'GuluRow',
+    props: {
+        gutter: {
+            type: [Number,String]
+        }
+    },
+    computed: {
+        rowStyle() {
+            let {gutter} =this;
+            return {
+                marginLeft: -gutter / 2 + 'px',
+                marginRight: -gutter / 2 + 'px'
+            }
+        }
+        },
+    mounted() {
+        this.$children.forEach((vm) => {
+            vm.gutter = this.gutter
+        })
+    }
+}
 </script>
 <style scoped lang="scss">
     .row{
